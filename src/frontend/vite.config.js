d79f804b2f1dbc16d7dfc8a1dddfdeadf2c18e92
Tamir Bahar <tamir.bahar@gmail.com>
2024-09-04 14:44:16 +0300

    Minor cleanup

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-// import { wasm } from '@rollup/plugin-wasm';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()
-  ]
-  ,
-  // optimizeDeps: {
-  //   exclude: ['*.wasm']
-  // },
-  // assetsInclude: ['*.wasm']
+  plugins: [svelte()],
 })
