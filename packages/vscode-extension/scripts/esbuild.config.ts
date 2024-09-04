4a8ae1c4f86888a3a337eb2094206f7510d7e9d0
Tamir Bahar <tamir.bahar@gmail.com>
2024-09-04 11:36:01 +0300

    Run bun build task

---
+++ packages/vscode-extension/scripts/esbuild.config.ts
@@ -0,0 +1,42 @@
+import type { BuildOptions } from "esbuild";
+// import { copy } from 'esbuild-plugin-copy';
+
+const config: BuildOptions = {
+  entryPoints: ["./src/extension.ts"],
+  bundle: true,
+  platform: "node",
+  target: "node12",
+  outdir: "./dist",
+  outbase: "./src",
+  outExtension: {
+    ".js": ".cjs",
+  },
+  format: "cjs",
+  external: ["vscode"],
+  loader: {
+    ".ts": "ts",
+    ".js": "js",
+    ".wasm": "file",
+  },
+  logLevel: "info",
+  // plugins: [
+  //   copy({
+  //     resolveFrom: 'cwd',
+  //     watch: true,
+  //     verbose: true,
+  //     assets: [
+  //       {
+  //         from: ['./parsers/*'],
+  //         to: ['./dist/'],
+  //       },
+  //       {
+
+  //         from: ["./media/*"],
+  //         to: ["./dist/media/"],
+  //       }
+  //     ],
+  //   }),
+  // ]
+};
+
+export default config;
