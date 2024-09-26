600f8e6e0f3ac13d34111158ac0655c397dbdaf5
Tamir Bahar <tamir.bahar@gmail.com>
2024-09-26 17:23:27 +0300

    Important todo note

--- src/vscode/extension.ts
+++ src/vscode/extension.ts
@@ -228,7 +228,8 @@ export async function activate(context: vscode.ExtensionContext) {
 
         savedCFG = cfg;
       }
-
+      // TODO: Highlighting in the DOT is a cute trick, but might become less effective on larger functions.
+      //       So it works for now, but I'll probably need to replace it with CSS so that I only render once per function.
       const nodeToHighlight = highlightCurrentNode ? getValue(cfg.offsetToNode, offset) : undefined;
       const dot = graphToDot(cfg, false, nodeToHighlight);
       const svg = graphviz.dot(dot);
