f2452de7032f06dd2fcffe48dddb5f7b6c124b44
Tamir Bahar <tamir.bahar@gmail.com>
2024-09-28 10:50:08 +0300

    frontend: write a Svelte component to render a DOT

---
+++ src/frontend/src/lib/DotRender.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { Graphviz } from "@hpcc-js/wasm-graphviz";
+  import { initialize as initializeUtils } from "./utils";
+
+  let graphviz: Graphviz;
+  export let dot: string;
+
+  async function initialize() {
+    const utils = await initializeUtils();
+    graphviz = utils.graphviz;
+  }
+</script>
+
+<div class="graph">
+  {#await initialize() then}
+    {@html graphviz.dot(dot)}
+  {/await}
+</div>
+
+<style>
+  .graph {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 1em;
+  }
+</style>
