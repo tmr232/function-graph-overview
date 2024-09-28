3e47aca568079065701540de0c6c9aedc0db4a53
Tamir Bahar <tamir.bahar@gmail.com>
2024-09-28 11:37:50 +0300

    tests: generate a full report of the test run, instead of just collecting the tests.
    Now I need to make the viewer load that...

--- scripts/collect-comment-tests.ts
+++ scripts/collect-comment-tests.ts
@@ -1,7 +1,10 @@
-import { intoRecords } from "../src/test/commentTestUtils";
 import { watch } from "fs";
 import { parseArgs } from "util";
 import { collectTests } from "../src/test/commentTestCollector";
+import type { TestFunction } from "../src/test/commentTestTypes";
+import { buildSimpleCFG, requirementTests } from "../src/test/commentTestHandlers";
+import { graphToDot } from "../src/control-flow/render";
+import { formatSnapshotName, loadDOTSnapshots } from "./generate-dot-snapshots";
 
 const watchDir = import.meta.dir + "/../src";
 
@@ -17,31 +20,86 @@ const { values } = parseArgs({
   allowPositionals: true,
 });
 
-async function generateJson() {
+export interface TestResults {
+  reqName: string;
+  reqValue: unknown;
+  failure: string | null;
+}
+
+function runTestsFor(testFunc: TestFunction): TestResults[] {
+  const testResults: TestResults[] = [];
+  for (const [key, value] of Object.entries(testFunc.reqs)) {
+    const reqHandler = requirementTests[key];
+    if (!reqHandler) {
+      continue;
+    }
+    testResults.push({
+      reqName: key,
+      reqValue: value,
+      failure: reqHandler(testFunc),
+    });
+  }
+  return testResults;
+}
+
+function generateDOTFor(testFunc: TestFunction): string {
+  const cfg = buildSimpleCFG(testFunc.language, testFunc.function);
+  const dot = graphToDot(cfg);
+  return dot;
+}
+
+type TestReport = {
+  name: string,
+  failed: boolean,
+  dot: { snapshot: string, current: string },
+  results: TestResults[],
+}
+
+async function generateReport() {
+  const testReports: TestReport[] = [];
+  const dotSnapshots = await loadDOTSnapshots();
+  const tests = await collectTests();
+  for (const testFunc of tests) {
+    const results = runTestsFor(testFunc);
+    const dot = {
+      current: generateDOTFor(testFunc),
+      snapshot: dotSnapshots[formatSnapshotName(testFunc)] as string
+    };
+    const failed = results.some(result => result.failure);
+    const name = `${testFunc.language}: ${testFunc.name}`
+    testReports.push({ dot, results, failed, name })
+  }
+  Bun.write("./dist/tests/testReport.json", JSON.stringify(testReports));
+}
+
+async function logAndContinue(fn: () => Promise<void>): Promise<void> {
   try {
-    const records = intoRecords(await collectTests());
-    Bun.write("./dist/tests/commentTests.json", JSON.stringify(records));
+    await fn();
   } catch (error) {
-    console.log(error);
+    console.trace(error);
   }
 }
 
-generateJson();
-if (values.watch) {
-  const watcher = watch(
-    watchDir,
-    { recursive: true },
-    async (event, filename) => {
-      console.log(`${event}: ${filename}, regenerating commentTests.json`);
-      await generateJson();
-    },
-  );
+async function main() {
+  await logAndContinue(generateReport);
+  if (values.watch) {
+    const watcher = watch(
+      watchDir,
+      { recursive: true },
+      async (event, filename) => {
+        console.log(`${event}: ${filename}, regenerating commentTests.json`);
+        await logAndContinue(generateReport);
+      },
+    );
 
-  process.on("SIGINT", () => {
-    // close watcher when Ctrl-C is pressed
-    console.log("Closing watcher...");
-    watcher.close();
+    process.on("SIGINT", () => {
+      // close watcher when Ctrl-C is pressed
+      console.log("Closing watcher...");
+      watcher.close();
 
-    process.exit(0);
-  });
+      process.exit(0);
+    });
+  }
 }
+
+await main();
--- scripts/generate-dot-snapshots.ts
+++ scripts/generate-dot-snapshots.ts
@@ -1,6 +1,18 @@
 import { collectTests } from "../src/test/commentTestCollector";
 import { buildSimpleCFG } from "../src/test/commentTestHandlers";
 import { graphToDot } from "../src/control-flow/render";
+import type { TestFunction } from "../src/test/commentTestTypes";
+
+export const dotSnapshotPath = "./src/test/__snapshots__/dotSnapshots.json";
+
+export async function loadDOTSnapshots(): Promise<Record<string, string>> {
+  const snapshotFile = Bun.file(dotSnapshotPath);
+  return JSON.parse(await snapshotFile.text())
+}
+
+export function formatSnapshotName(testFunc:TestFunction):string {
+  return `${testFunc.language}: ${testFunc.name}`
+}
 
 async function generateSnapshots() {
   const tests = await collectTests();
@@ -8,10 +20,10 @@ async function generateSnapshots() {
   for (const testFunc of tests) {
     const cfg = buildSimpleCFG(testFunc.language, testFunc.function);
     const dot = graphToDot(cfg);
-    snapshots[`${testFunc.language}: ${testFunc.name}`] = dot;
+    snapshots[formatSnapshotName(testFunc)] = dot;
   }
   await Bun.write(
-    "./src/test/__snapshots__/dotSnapshots.json",
+    dotSnapshotPath,
     JSON.stringify(snapshots),
   );
 }
