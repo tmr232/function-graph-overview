cc0b1e75bb52d95c37133dec28dd067317136b6c
Tamir Bahar <tamir.bahar@gmail.com>
2024-10-09 13:19:59 +0300

    demo: Ability to export, import, and reset color scheme

--- src/components/ColorScheme.svelte
+++ src/components/ColorScheme.svelte
@@ -4,60 +4,111 @@
 
   const dispatch = createEventDispatcher();
 
-  const colorScheme = [
+  const defaultColorScheme = [
     // Node colors
-    { name: "node.default", label: "Default", hex: "#d3d3d3" },
-    { name: "node.entry", label: "Entry", hex: "#48AB30" },
-    { name: "node.exit", label: "Exit", hex: "#AB3030" },
-    { name: "node.throw", label: "Throw", hex: "#ffdddd" },
-    { name: "node.yield", label: "Yield", hex: "#00bfff" },
-    { name: "node.border", label: "Border", hex: "#000000" },
-    { name: "node.highlight", label: "Highlight", hex: "#000000" },
+    { name: "node.default", hex: "#d3d3d3" },
+    { name: "node.entry", hex: "#48AB30" },
+    { name: "node.exit", hex: "#AB3030" },
+    { name: "node.throw", hex: "#ffdddd" },
+    { name: "node.yield", hex: "#00bfff" },
+    { name: "node.border", hex: "#000000" },
+    { name: "node.highlight", hex: "#000000" },
 
     // Edge Colors
-    { name: "edge.regular", label: "Regular", hex: "#0000ff" },
-    { name: "edge.consequence", label: "Consequence", hex: "#008000" },
-    { name: "edge.alternative", label: "Alternative", hex: "#ff0000" },
+    { name: "edge.regular", hex: "#0000ff" },
+    { name: "edge.consequence", hex: "#008000" },
+    { name: "edge.alternative", hex: "#ff0000" },
 
     // Cluster Colors
-    { name: "cluster.border", label: "Border", hex: "#ffffff" },
-    { name: "cluster.with", label: "With", hex: "#ffddff" },
-    { name: "cluster.tryComplex", label: "Try Complex", hex: "#ddddff" },
-    { name: "cluster.try", label: "Try", hex: "#ddffdd" },
-    { name: "cluster.finally", label: "Finally", hex: "#ffffdd" },
-    { name: "cluster.except", label: "Except", hex: "#ffdddd" },
+    { name: "cluster.border", hex: "#ffffff" },
+    { name: "cluster.with", hex: "#ffddff" },
+    { name: "cluster.tryComplex", hex: "#ddddff" },
+    { name: "cluster.try", hex: "#ddffdd" },
+    { name: "cluster.finally", hex: "#ffffdd" },
+    { name: "cluster.except", hex: "#ffdddd" },
 
     // Graph Colors
-    { name: "graph.background", label: "Background", hex: "#00000000" },
-  ];
+    { name: "graph.background", hex: "#00000000" },
+  ] as const;
+
+  let colorScheme = structuredClone(defaultColorScheme);
+
+  const colorLabels = new Map([
+    ["node.default", "Default"],
+    ["node.entry", "Entry"],
+    ["node.exit", "Exit"],
+    ["node.throw", "Throw"],
+    ["node.yield", "Yield"],
+    ["node.border", "Border"],
+    ["node.highlight", "Highlight"],
+    ["edge.regular", "Regular"],
+    ["edge.consequence", "Consequence"],
+    ["edge.alternative", "Alternative"],
+    ["cluster.border", "Border"],
+    ["cluster.with", "With"],
+    ["cluster.tryComplex", "Try Complex"],
+    ["cluster.try", "Try"],
+    ["cluster.finally", "Finally"],
+    ["cluster.except", "Except"],
+    ["graph.background", "Background"],
+  ]) as const;
 
   const groups = ["Node", "Edge", "Cluster", "Graph"] as const;
 
-  function colorsFor(entity: (typeof groups)[number]) {
-    return colorScheme.filter(({ name }) =>
-      name.startsWith(entity.toLowerCase()),
-    );
+  function colorsFor(
+    scheme: typeof defaultColorScheme,
+    entity: (typeof groups)[number],
+  ) {
+    return scheme.filter(({ name }) => name.startsWith(entity.toLowerCase()));
   }
 
-  let configInput;
+  function serializeColorScheme(
+    scheme: { name: string; hex: string }[],
+  ): string {
+    return JSON.stringify({ version: 1, scheme });
+  }
+
+  function deserializeColorScheme(data: string) {
+    const { version, scheme } = JSON.parse(data);
+    if (version !== 1) {
+      throw new Error(`Invalid scheme version: ${version}`);
+    }
+    for (const { hex } of scheme) {
+      if (!hex.match(/^#[0-9a-fA-F]+$/)) {
+        throw new Error(`Invalid color: ${hex}`);
+      }
+    }
+    return scheme;
+  }
+
   function onColorChange(color) {
     return (event) => {
       color.hex = event.detail.hex ?? color.hex;
       dispatch("color", { colors: colorScheme });
-      // console.log(configInput);
-      if (configInput) {
-        configInput.value = JSON.stringify(colorScheme);
-      }
     };
   }
+
+  async function copyScheme() {
+    await navigator.clipboard.writeText(serializeColorScheme(colorScheme));
+  }
+  async function pasteScheme() {
+    const newScheme = deserializeColorScheme(
+      await navigator.clipboard.readText(),
+    );
+    colorScheme = newScheme;
+  }
+  function resetScheme() {
+    colorScheme = structuredClone(defaultColorScheme);
+  }
+  function applyScheme() {}
 </script>
 
 {#each groups as group}
   <fieldset>
     <legend>{group}</legend>
     <div class="colors">
-      {#each colorsFor(group) as color (color.name)}
-        <span>{color.label}</span>
+      {#each colorsFor(colorScheme, group) as color (color.name)}
+        <span>{colorLabels.get(color.name)}</span>
         <div class="border">
           <ColorPicker
             hex={color.hex}
@@ -73,7 +124,10 @@
 {/each}
 <fieldset>
   <legend>As Text</legend>
-  <input type="text" bind:this={configInput} />
+  <button on:click={copyScheme}>Copy</button>
+  <button on:click={pasteScheme}>Paste</button>
+  <button on:click={resetScheme}>Reset</button>
+  <button on:click={applyScheme}>Apply</button>
 </fieldset>
 
 <style>
