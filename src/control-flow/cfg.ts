a7f0c1467e9b603613eb815ec6a10035bd87ddd2
Tamir Bahar <tamir.bahar@gmail.com>
2024-11-15 21:25:00 +0200

    Formatting & Linting

--- src/control-flow/cfg.ts
+++ src/control-flow/cfg.ts
@@ -3,12 +3,11 @@ import type { BuilderOptions, CFGBuilder } from "./cfg-defs";
 import { createCFGBuilder as createGoCFGBuilder } from "./cfg-go";
 import { createCFGBuilder as createPythonCFGBuilder } from "./cfg-python";
 
-const supportedLanguages = ["C", "Go", "Python"] as const
-export type Language = typeof supportedLanguages[number];
+const supportedLanguages = ["C", "Go", "Python"] as const;
+export type Language = (typeof supportedLanguages)[number];
 export function isValidLanguage(language: string): language is Language {
-  return (supportedLanguages as readonly string[]).includes(language)
+  return (supportedLanguages as readonly string[]).includes(language);
 }
-
 
 export function newCFGBuilder(
   language: Language,
--- src/jetbrains/vite.config.js
+++ src/jetbrains/vite.config.js
@@ -10,5 +10,4 @@ export default defineConfig({
     emptyOutDir: true,
   },
   base: "",
-
 });
