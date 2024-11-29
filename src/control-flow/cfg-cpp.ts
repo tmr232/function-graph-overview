26099b1fe3d12f241ee05ecd73fcb364b80a6682
Tamir Bahar <tamir.bahar@gmail.com>
2024-11-29 21:11:35 +0200

    cpp: Support try-catch

--- src/control-flow/cfg-cpp.ts
+++ src/control-flow/cfg-cpp.ts
@@ -32,6 +32,7 @@ const statementHandlers: StatementHandlers = {
     labeled_statement: processLabeledStatement,
     goto_statement: processGotoStatement,
     comment: processComment,
+    try_statement: processTryStatement,
   },
   default: defaultProcessStatement,
 };
@@ -557,4 +558,77 @@ function processSwitchlike(
   }
 
   return blockHandler.update({ entry: headNode, exit: mergeNode });
+}
+
+function processTryStatement(
+  trySyntax: Parser.SyntaxNode,
+  ctx: Context,
+): BasicBlock {
+  const { builder, matcher } = ctx;
+  /*
+  Here's an idea - I can duplicate the finally blocks!
+  Then if there's a return, I stick the finally before it.
+  In other cases, the finally is after the end of the try-body.
+  This is probably the best course of action.
+  */
+  const match = matcher.match(
+    trySyntax,
+    `
+      (try_statement
+      body: (_) @try-body
+          (catch_clause body: (_) @catch-body)* @catch
+      ) @try
+      `,
+  );
+
+  const bodySyntax = match.requireSyntax("try-body");
+  const catchSyntaxMany = match.getSyntaxMany("catch-body");
+
+  const mergeNode = builder.addNode(
+    "MERGE",
+    "merge try-complex",
+    trySyntax.endIndex,
+  );
+
+  return builder.withCluster("try-complex", (_tryComplexCluster) => {
+    const bodyBlock = builder.withCluster("try", () =>
+      match.getBlock(bodySyntax),
+    );
+    ctx.link.syntaxToNode(trySyntax, bodyBlock.entry);
+
+    // We handle `except` blocks before the `finally` block to support `return` handling.
+    const exceptBlocks = catchSyntaxMany.map((exceptSyntax) =>
+      builder.withCluster("except", () => match.getBlock(exceptSyntax)),
+    );
+    for (const [syntax, { entry }] of zip(
+      match.getSyntaxMany("except"),
+      exceptBlocks,
+    )) {
+      ctx.link.syntaxToNode(syntax, entry);
+    }
+    // We attach the except-blocks to the top of the `try` body.
+    // In the rendering, we will connect them to the side of the node, and use invisible lines for it.
+    if (bodyBlock.entry) {
+      const headNode = bodyBlock.entry;
+      for (const exceptBlock of exceptBlocks) {
+        // Yes, this is effectively a head-to-head link. But that's ok.
+        builder.addEdge(headNode, exceptBlock.entry, "exception");
+      }
+    }
+
+    // This is the exit we get to if we don't have an exception
+
+    // We need to connect the `except` blocks to the merge node
+    for (const exceptBlock of exceptBlocks) {
+      if (exceptBlock.exit) builder.addEdge(exceptBlock.exit, mergeNode);
+    }
+    const happyExit: string | null = bodyBlock.exit;
+
+    if (happyExit) builder.addEdge(happyExit, mergeNode);
+
+    return matcher.update({
+      entry: bodyBlock.entry,
+      exit: mergeNode,
+    });
+  });
 }
