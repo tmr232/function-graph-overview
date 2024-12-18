a99ff11ca001c0b26338d2a35b7bb2b36c44495a
Tamir Bahar <tamir.bahar@gmail.com>
2024-12-18 12:08:15 +0200

    Demo: Add nice typescript demo code

--- src/demo/src/assets/demo.ts
+++ src/demo/src/assets/demo.ts
@@ -1,3 +1,15 @@
-function main() {
-  return;
+export function* iterRanges<T>(
+  ranges: SimpleRange<T>[],
+): Generator<{ start: number; stop?: number; value: T }> {
+  let i = 0;
+  for (; i < ranges.length - 1; ++i) {
+    yield {
+      start: ranges[i].start,
+      stop: ranges[i + 1]?.start,
+      value: ranges[i].value,
+    };
+  }
+  if (i < ranges.length) {
+    yield { start: ranges[i].start, value: ranges[i].value };
+  }
 }
