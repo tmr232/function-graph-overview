5872e4306b59a1931a3704626383ce51729fdc64
Tamir Bahar <tamir.bahar@gmail.com>
2024-12-31 18:41:53 +0200

    Publish the render website

--- src/demo/vite.config.js
+++ src/demo/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [svelte()],
   build: {
     copyPublicDir: true,
+    outDir: "../../dist/demo",
     emptyOutDir: true,
   },
   base: "/function-graph-overview/",
