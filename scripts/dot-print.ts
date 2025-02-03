6cc22e3fe2c410c7d3508fa48230ae13cdb73f58
Tamir Bahar <tamir.bahar@gmail.com>
2025-02-03 23:53:48 +0200

    I think that's everything except clusters

--- scripts/dot-print.ts
+++ scripts/dot-print.ts
@@ -1,4 +1,4 @@
-import { fromDot, attribute, toDot, type NodeAttributesObject } from "ts-graphviz";
+import { fromDot, attribute, toDot, type NodeAttributesObject, type EdgeAttributesObject, Edge } from "ts-graphviz";
 import { type ColorScheme, getDefaultColorScheme } from "../src/control-flow/colors.ts";
 
 
@@ -26,6 +26,24 @@ function getNodeAttributes(colorScheme:ColorScheme) {
   return nodeAttributes;
 }
 
+function getEdgeClassAttributes(colorScheme:ColorScheme) {
+  const edgeAttributes: { [key: string]: EdgeAttributesObject } = {
+    consequence: {
+      [attribute.color]: colorScheme["edge.consequence"],
+    },
+    alternative: {
+      [attribute.color]: colorScheme["edge.alternative"],
+    },
+    exception: {
+      [attribute.style]: "invis",
+      [attribute.headport]:"e",
+      [attribute.tailport]:"w",
+    },
+  }
+
+  return edgeAttributes;
+}
+
 export function applyTheme(dot:string, colorScheme:ColorScheme):string {
   const G = fromDot(dot);
   G.apply({
@@ -42,7 +60,10 @@ export function applyTheme(dot:string, colorScheme:ColorScheme):string {
     [attribute.color]:colorScheme["edge.regular"],
     [attribute.headport]:"n",
     [attribute.tailport]:"s",
+    [attribute.penwidth]:1,
   })
+
+
   const allNodeAttributes = getNodeAttributes(colorScheme);
   for (const node of G.nodes) {
     for (const cls of (node.attributes.get(attribute.class)??"").split(/\s/)) {
@@ -53,6 +74,24 @@ export function applyTheme(dot:string, colorScheme:ColorScheme):string {
     }
   }
 
+  const edgeClassAttributes = getEdgeClassAttributes(colorScheme);
+  for (const edge of G.edges) {
+    for (const cls of (edge.attributes.get(attribute.class)??"").split(/\s/)) {
+      const attrObj = edgeClassAttributes[cls];
+      if (attrObj) {
+        edge.attributes.apply(attrObj);
+      }
+    }
+    if (edge.attributes.get(attribute.dir) === "back") {
+      edge.attributes.apply({
+        [attribute.penwidth]:2,
+        [attribute.headport]:"s",
+        [attribute.tailport]:"n",
+      });
+      edge.targets.reverse();
+    }
+  }
+
   return toDot(G);
 }
 
@@ -60,4 +99,5 @@ console.log(applyTheme(  `digraph {
     a [class="entry"]
     b [class="exit"]
     a -> b [class="default"]
+    b -> a [class="alternative" dir="back"]
   }`, getDefaultColorScheme()))
