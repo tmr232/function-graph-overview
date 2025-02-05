501c35e3e67e1062517033c1e8dffa5172b32a4a
Tamir Bahar <tamir.bahar@gmail.com>
2025-02-05 14:12:10 +0200

    Add support for continue with label in Go
    Closes #85

--- src/test/commentTestSamples/labledStatements.go
+++ src/test/commentTestSamples/labledStatements.go
@@ -12,3 +12,19 @@ func LabelledStatementFlow() {
     unreachable_from_inner_loop()
   }
 }
+
+/*
+reach: [["inner", "outer"]]
+*/
+func ContinueToLabel() {
+outer:
+	for i := 0; i < 5; i++ {
+	    // CFG: outer
+		fmt.Println(i)
+		for {
+		    // CFG: inner
+			continue outer
+		}
+		fmt.Println("Oh no!")
+	}
+}
