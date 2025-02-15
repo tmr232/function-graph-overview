d18ecc0b74c68141c8d4eba85dfb84d2e657154b
Tamir Bahar <tamir.bahar@gmail.com>
2025-02-15 16:54:13 +0200

    More examples!

--- scripts/dot-print.ts
+++ scripts/dot-print.ts
@@ -1,4 +1,5 @@
 import * as LZString from "lz-string";
+import { Graphviz } from "@hpcc-js/wasm-graphviz";
 import {
   type EdgeAttributesObject,
   type EdgeModel,
@@ -18,10 +19,12 @@ function getNodeAttributes(colorScheme: ColorScheme) {
     entry: {
       [attribute.fillcolor]: colorScheme["node.entry"],
       [attribute.shape]: "invhouse",
+      [attribute.height]: 0.5,
     },
     exit: {
       [attribute.fillcolor]: colorScheme["node.exit"],
       [attribute.shape]: "house",
+      [attribute.height]: 0.5,
     },
     throw: {
       [attribute.fillcolor]: colorScheme["node.throw"],
@@ -74,6 +77,7 @@ export function applyTheme(dot: string, colorScheme: ColorScheme): string {
     [attribute.fillcolor]: colorScheme["node.default"],
     [attribute.color]: colorScheme["node.border"],
     [attribute.fontname]: "sans-serif",
+    [attribute.height]: 0.3,
   });
   G.edge({
     [attribute.color]: colorScheme["edge.regular"],
@@ -151,13 +155,121 @@ const graphExamples = {
   `,
 };
 
+const gradual = [
+  "body",
+  `
+  stmt1 -> stmt2
+  stmt2 -> stmt3
+  stmt3 -> stmt4
+  `,
+  `
+  stmt1 -> then1 [class="consequence"]
+  stmt1 -> else1 [class="alternative"]
+  then1 -> stmt2
+  else1 -> stmt2
+  stmt2 -> stmt3
+  stmt3 -> stmt4
+  `,
+  `
+  return1 [class="exit"]
+  stmt1 -> return1 [class="consequence"]
+  stmt1 -> else1 [class="alternative"]
+  else1 -> stmt2
+  stmt2 -> stmt3
+  stmt3 -> stmt4
+  `,
+  `
+  return1 [class="exit"]
+  return2 [class="exit"]
+  stmt1 -> return1 [class="consequence"]
+  stmt1 -> else1 [class="alternative"]
+  else1 -> stmt2
+  stmt2 -> return2 [class="consequence"]
+  stmt2 -> else2 [class="alternative"]
+  else2 -> stmt3
+  stmt3 -> stmt4
+  `,
+  `
+  return1 [class="exit"]
+  return2 [class="exit"]
+  stmt1 -> return1 [class="consequence"]
+  stmt1 -> else1 [class="alternative"]
+  else1 -> stmt2
+  stmt2 -> return2 [class="consequence"]
+  stmt2 -> else2 [class="alternative"]
+  else2 -> stmt3
+  stmt3 -> stmt4
+  stmt4 -> stmt4 [dir="back"]
+  `,
+  `
+  return1 [class="exit"]
+  return2 [class="exit"]
+  stmt1 -> return1 [class="consequence"]
+  stmt1 -> else1 [class="alternative"]
+  else1 -> stmt2
+  stmt2 -> return2 [class="consequence"]
+  stmt2 -> else2 [class="alternative"]
+  else2 -> stmt3
+  stmt3 -> stmt4_1
+  stmt4_1 -> stmt4_2
+  stmt4_2 -> stmt4_3
+  stmt4_3 -> stmt4_4
+  stmt4_4 -> stmt4_1 [dir="back"]
+  `,
+  `
+  return1 [class="exit"]
+  return2 [class="exit"]
+  return4_2 [class="exit"]
+  stmt1 -> return1 [class="consequence"]
+  stmt1 -> else1 [class="alternative"]
+  else1 -> stmt2
+  stmt2 -> return2 [class="consequence"]
+  stmt2 -> else2 [class="alternative"]
+  else2 -> stmt3
+  stmt3 -> stmt4_1
+  stmt4_1 -> stmt4_2
+  stmt4_2 -> return4_2 [class="consequence"]
+  stmt4_2 -> else4_2 [class="alternative"]
+  else4_2 -> stmt4_3
+  stmt4_3 -> stmt4_4
+  stmt4_4 -> stmt4_1 [dir="back"]
+  `,
+  `
+  entry [class="entry"]
+  return1 [class="exit"]
+  return2 [class="exit"]
+  return4_2 [class="exit"]
+  entry -> stmt1
+  stmt1 -> return1 [class="consequence"]
+  stmt1 -> else1 [class="alternative"]
+  else1 -> stmt2
+  stmt2 -> return2 [class="consequence"]
+  stmt2 -> else2 [class="alternative"]
+  else2 -> stmt3
+  stmt3 -> stmt4_1
+  stmt4_1 -> stmt4_2
+  stmt4_2 -> return4_2 [class="consequence"]
+  stmt4_2 -> else4_2 [class="alternative"]
+  else4_2 -> stmt4_3
+  stmt4_3 -> stmt4_4
+  stmt4_4 -> stmt4_1 [dir="back"]
+  `
+]
+
 if (require.main === module) {
-  for (const [name, graphContent] of Object.entries(graphExamples)) {
-    console.log(
-      name,
-      editDotLink(
-        applyTheme(`digraph { ${graphContent} }`, getDefaultColorScheme()),
-      ),
-    );
+  // for (const [name, graphContent] of Object.entries(graphExamples)) {
+  //   console.log(
+  //     name,
+  //     editDotLink(
+  //       applyTheme(`digraph { ${graphContent} }`, getDefaultColorScheme()),
+  //     ),
+  //   );
+  // }
+  const graphviz = await Graphviz.load();
+  for (let i=0; i<gradual.length; ++i){
+    const step = gradual[i]
+    const styledDot = applyTheme(`digraph { ${step} }`, getDefaultColorScheme());
+    const svg = graphviz.dot(styledDot);
+    await Bun.write(`step_${i}.svg`, svg)
   }
 }
