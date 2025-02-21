5b1aaaeb9dfdc0ee121f666ca63a61124346a733
Tamir Bahar <tamir.bahar@gmail.com>
2025-02-21 14:11:26 +0200

    Move wasm paths to language definitions

--- src/parser-loader/wasmMapping.ts
+++ src/parser-loader/wasmMapping.ts
@@ -1,17 +1,12 @@
-import treeSitterC from "../../parsers/tree-sitter-c.wasm?url";
-import treeSitterCpp from "../../parsers/tree-sitter-cpp.wasm?url";
-import treeSitterGo from "../../parsers/tree-sitter-go.wasm?url";
-import treeSitterPython from "../../parsers/tree-sitter-python.wasm?url";
-import treeSitterTSX from "../../parsers/tree-sitter-tsx.wasm?url";
-import treeSitterTypeScript from "../../parsers/tree-sitter-typescript.wasm?url";
 import type { Language } from "../control-flow/cfg.ts";
+import { languageDefinitions } from "../control-flow/cfg.ts";
 
 // ADD-LANGUAGES-HERE
 export const wasmMapping: { [language in Language]: string } = {
-  C: treeSitterC,
-  Go: treeSitterGo,
-  Python: treeSitterPython,
-  "C++": treeSitterCpp,
-  TypeScript: treeSitterTypeScript,
-  TSX: treeSitterTSX,
+  C: languageDefinitions.C.wasmPath,
+  Go: languageDefinitions.Go.wasmPath,
+  Python: languageDefinitions.Python.wasmPath,
+  "C++": languageDefinitions["C++"].wasmPath,
+  TypeScript: languageDefinitions.TypeScript.wasmPath,
+  TSX: languageDefinitions.TSX.wasmPath,
 };
